f45024a00fa436b26202bc98f2b194d7ac6f9afa
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-26 21:47:07 -0300

    Ajust Ecto.Repo configuration for :dev and :test environments

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :bank, Bank.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_USERNAME", "postgres"),
+  password: System.get_env("DATABASE_PASSWORD", "postgres"),
   database: "bank_dev",
-  hostname: "localhost",
+  hostname: System.get_env("DATABASE_HOSTNAME", "localhost"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 

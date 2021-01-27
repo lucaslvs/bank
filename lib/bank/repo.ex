821c505a2cba9f00a08cc336cd8a1dda39c5fd66
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-26 21:48:44 -0300

    Implement Ecto.Repo.init/2 callback in Bank.Repo to put value of DATABASE_URL environment variable

--- lib/bank/repo.ex
+++ lib/bank/repo.ex
@@ -2,4 +2,9 @@ defmodule Bank.Repo do
   use Ecto.Repo,
     otp_app: :bank,
     adapter: Ecto.Adapters.Postgres
+
+  @impl Ecto.Repo
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end

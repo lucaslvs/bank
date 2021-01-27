5103ccc6661c940d01e075f56ca6607030251ff0
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-26 23:27:44 -0300

    Implements Bank.Customers.User.changeset/2 validations

--- lib/bank/customers/user.ex
+++ lib/bank/customers/user.ex
@@ -7,6 +7,9 @@ defmodule Bank.Customers.User do
 
   alias Bank.Customers.Account
 
+  @required_fields [:name, :email, :password]
+  @email_format ~r/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   @type t :: %__MODULE__{
     id: integer(),
     account: Bank.Customers.Account.t() | %Ecto.Association.NotLoaded{},
@@ -19,6 +22,7 @@ defmodule Bank.Customers.User do
   schema "users" do
     field :email, :string, null: false
     field :name, :string, null: false
+    field :password, :string, virtual: true
     field :password_hash, :string, null: false
 
     has_one :account, Account
@@ -29,8 +33,22 @@ defmodule Bank.Customers.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password_hash])
-    |> validate_required([:name, :email, :password_hash])
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_format(:email, @email_format)
+    |> validate_length(:name, min: 1)
+    |> validate_length(:password, min: 6)
     |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        change(changeset, Argon2.add_hash(password))
+
+      changeset ->
+        changeset
+    end
   end
 end

85b0ac3daf26254ab875798d065399daf331351b
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-29 00:48:56 -0300

    Refactor by Credo suggestions

--- lib/bank/notifications.ex
+++ lib/bank/notifications.ex
@@ -1,9 +1,9 @@
 defmodule Bank.Notifications do
   @moduledoc false
 
+  alias Bank.Customers.User
   alias Bank.Notifications.Mailer
   alias Bank.Notifications.UserAccountWithdrawMail
-  alias Bank.Customers.User
 
   @spec send_user_account_withdraw_email(Bank.Customers.User.t(), Money.t()) :: Bamboo.Email.t()
   def send_user_account_withdraw_email(%User{} = user, %Money{} = money) do

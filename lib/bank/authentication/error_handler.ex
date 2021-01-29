309b964204b03939709759440238119aaf48f99c
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-28 21:53:47 -0300

    Refactor Bank.Authentication.ErrorHandler to return a object

--- lib/bank/authentication/error_handler.ex
+++ lib/bank/authentication/error_handler.ex
@@ -7,6 +7,15 @@ defmodule Bank.Authentication.ErrorHandler do
   def auth_error(conn, {type, _reason}, _opts) do
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(401, to_string(type))
+    |> send_resp(401, resp_body(type))
+  end
+
+  defp resp_body(type) do
+    detail =
+      type
+      |> to_string()
+      |> String.capitalize()
+
+    Jason.encode!(%{errors: %{detail: detail}})
   end
 end

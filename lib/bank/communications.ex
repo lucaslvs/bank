7f84e135b22e39ced73e6231ccb897e2ffb74a74
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-29 00:27:07 -0300

    Creates Bank.Communications.send_user_account_withdraw_email/2 function

--- lib/bank/communications.ex
+++ lib/bank/communications.ex
@@ -1,2 +1,14 @@
 defmodule Bank.Communications do
+  @moduledoc false
+
+  alias Bank.Communications.Mailer
+  alias Bank.Communications.UserAccountWithdrawMail
+  alias Bank.Customers.User
+
+  @spec send_user_account_withdraw_email(Bank.Customers.User.t(), Money.t()) :: Bamboo.Email.t()
+  def send_user_account_withdraw_email(%User{} = user, %Money{} = money) do
+    user
+    |> UserAccountWithdrawMail.build(money)
+    |> Mailer.deliver_later()
+  end
 end

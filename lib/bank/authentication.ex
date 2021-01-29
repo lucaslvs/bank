adf72821d30b43c392825000e2a5445b69fa8a75
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-28 21:19:26 -0300

    Refator Bank.Authentication.authenticate_user/2 to returns a JWT token

--- lib/bank/authentication.ex
+++ lib/bank/authentication.ex
@@ -1,17 +1,23 @@
 defmodule Bank.Authentication do
   @moduledoc false
 
+  alias Bank.Authentication.Guardian
   alias Bank.Customers
   alias Bank.Customers.User
 
   @doc """
   Authenticate user by the given `email` and `password`.
+
+  Will returns a `Guardian.Token.token()` and `Guardian.Token.claims()`, If `email` and `password` is valid.
   """
-  @spec authenticate_user(binary(), binary()) :: {:ok, User.t()} | {:error, :unauthorized}
+  @spec authenticate_user(binary(), binary()) ::
+          {:ok, Guardian.Token.token(), Guardian.Token.claims()}
+          | {:error, any()}
+          | {:error, :unauthorized}
   def authenticate_user(email, password) when is_binary(email) and is_binary(password) do
     with {:ok, %User{} = user} <- Customers.get_user_by(%{email: email}),
          true <- is_valid_user_password?(user, password) do
-      {:ok, user}
+      Guardian.encode_and_sign(user)
     else
       {:error, :not_found} ->
         Argon2.no_user_verify()

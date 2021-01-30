b847089e5a84bddc091e9d07b1e6c26839cad3ea
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-29 21:03:33 -0300

    Apply formatter

--- test/bank/customers_test.exs
+++ test/bank/customers_test.exs
@@ -6,6 +6,52 @@ defmodule Bank.CustomersTest do
   alias Bank.Customers
   alias Bank.Customers.{Account, User}
 
+  # describe "open_account/1" do
+  #   setup [:create_user, :create_account]
+
+  #   test "" do
+
+  #   end
+
+  #   test "" do
+
+  #   end
+  # end
+
+  describe "get_account!/1" do
+    setup [:create_user, :create_account]
+
+    test "Returns the account when given id is valid", %{account: account_expected} do
+      account_received = Customers.get_account!(account_expected.id)
+
+      assert account_expected.id == account_received.id
+      assert account_expected.number == account_received.number
+      assert account_expected.balance == account_received.balance
+    end
+
+    test "Raise a error when given id is invalid", %{account: %Account{id: id}} do
+      assert_raise Ecto.NoResultsError, fn ->
+        Customers.get_account!(id + 1)
+      end
+    end
+  end
+
+  describe "get_account/1" do
+    setup [:create_user, :create_account]
+
+    test "Returns the account when given id is valid", %{account: account_expected} do
+      {:ok, %Account{} = account_received} = Customers.get_account(account_expected.id)
+
+      assert account_expected.id == account_received.id
+      assert account_expected.number == account_received.number
+      assert account_expected.balance == account_received.balance
+    end
+
+    test "Raise a error when given id is invalid", %{account: %Account{id: id}} do
+      assert {:error, :not_found} = Customers.get_account(id + 1)
+    end
+  end
+
   describe "get_user!/1" do
     setup :create_user
 
@@ -75,52 +121,6 @@ defmodule Bank.CustomersTest do
       params = Map.new(id: id + 1, name: "not exist", email: "not exist")
 
       assert {:error, :not_found} = Customers.get_user_by(params)
-    end
-  end
-
-  # describe "open_account/1" do
-  #   setup [:create_user, :create_account]
-
-  #   test "" do
-
-  #   end
-
-  #   test "" do
-
-  #   end
-  # end
-
-  describe "get_account!/1" do
-    setup [:create_user, :create_account]
-
-    test "Returns the account when given id is valid", %{account: account_expected} do
-      account_received = Customers.get_account!(account_expected.id)
-
-      assert account_expected.id == account_received.id
-      assert account_expected.number == account_received.number
-      assert account_expected.balance == account_received.balance
-    end
-
-    test "Raise a error when given id is invalid", %{account: %Account{id: id}} do
-      assert_raise Ecto.NoResultsError, fn ->
-        Customers.get_account!(id + 1)
-      end
-    end
-  end
-
-  describe "get_account/1" do
-    setup [:create_user, :create_account]
-
-    test "Returns the account when given id is valid", %{account: account_expected} do
-      {:ok, %Account{} = account_received} = Customers.get_account(account_expected.id)
-
-      assert account_expected.id == account_received.id
-      assert account_expected.number == account_received.number
-      assert account_expected.balance == account_received.balance
-    end
-
-    test "Raise a error when given id is invalid", %{account: %Account{id: id}} do
-      assert {:error, :not_found} = Customers.get_account(id + 1)
     end
   end
 

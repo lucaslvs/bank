5934198ff7e4a3df561b6d74e084878c50e4d424
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-01-30 00:25:22 -0300

    Add test for Bank.Notifications.send_user_account_withdraw_email/2 function

---
+++ test/bank/notifications_test.exs
@@ -0,0 +1,40 @@
+defmodule Bank.NotificationsTest do
+  use Bank.DataCase, async: true
+  use Bamboo.Test
+
+  alias Bamboo.Email
+  alias Bank.Customers.User
+  alias Bank.Notifications
+  alias Bank.Notifications.{UserAccountWithdrawEmail, Mailer}
+
+  import Bank.Factory
+  import Mock
+
+  setup [:create_user, :create_account]
+
+  describe "send_user_account_withdraw_email/2" do
+    test "Sends a user account withdraw email to received user email", %{user: user} do
+      build_mock = fn %User{}, %Money{} -> %Email{} end
+      deliver_later_mock = fn %Email{} -> :ok end
+
+      mocks = [
+        {UserAccountWithdrawEmail, [], build: build_mock},
+        {Mailer, [], deliver_later: deliver_later_mock}
+      ]
+
+      with_mocks mocks do
+        money = Money.new(1_000)
+        email = UserAccountWithdrawEmail.build(user, money)
+
+        Notifications.send_user_account_withdraw_email(user, money)
+
+        assert_called UserAccountWithdrawEmail.build(user, money)
+        assert_called Mailer.deliver_later(email)
+      end
+    end
+  end
+
+  defp create_user(_context), do: {:ok, user: insert(:user)}
+
+  defp create_account(%{user: user}), do: {:ok, account: insert(:account, user: user)}
+end

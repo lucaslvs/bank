e27c5fe73905f0c7bbe37b444e9ca136bb0b027e
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-02 20:52:25 -0300

    Creates Bank.Financial.Withdraw operation

---
+++ lib/bank/financial/operation.ex
@@ -0,0 +1,23 @@
+defmodule Bank.Financial.Operation do
+  @moduledoc """
+  A module that map definitions used by `Bank.Financial` context.
+
+  This can be used in your application as:
+
+      use Bank.Financial.Operation
+  """
+
+  @callback build(map()) :: Ecto.Multi.t()
+
+  def multi do
+    quote do
+      @behaviour Bank.Financial.Operation
+
+      alias Ecto.Multi
+    end
+  end
+
+  defmacro __using__(_) do
+    apply(__MODULE__, :multi, [])
+  end
+end

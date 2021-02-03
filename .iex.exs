1eec583f4d3700dc937fab908a56683e4acb8a1e
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-02 21:06:30 -0300

    Updates .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,7 +1,8 @@
 import Ecto.Query, warn: false
 
-alias Bank.Customers
+alias Bank.{Customers, Financial}
 alias Bank.Customers.{Account, User}
+alias Bank.Financial.Transaction
 alias Bank.Repo
 alias BankWeb.Authentication
 alias BankWeb.Router.Helpers, as: Routes

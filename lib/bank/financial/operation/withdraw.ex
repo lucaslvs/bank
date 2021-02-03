f499febb576c5f64ef2bb9a4d4e01b035bc42fc3
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-03 01:16:59 -0300

    Refactor Bank.Financial.Operation.Withdraw private functions

--- lib/bank/financial/operation/withdraw.ex
+++ lib/bank/financial/operation/withdraw.ex
@@ -9,19 +9,18 @@ defmodule Bank.Financial.Operation.Withdraw do
   @impl Bank.Financial.Operation
   @spec build(%{account: Account.t(), amount: Money.t()}) :: Multi.t()
   def build(%{account: %Account{} = account, amount: %Money{} = amount}) do
-    withdrawal_transaction = &withdraw_changeset(Map.get(&1, :withdrawal_account), amount)
-
     Multi.new()
-    |> Multi.update(:withdrawal_account, update_account_changeset(account, amount))
-    |> Multi.insert(:withdrawal_transaction, withdrawal_transaction)
+    |> Multi.update(:withdrawal_account, update_account_balance(account, amount))
+    |> Multi.insert(:withdrawal_transaction, &create_transaction(&1, amount))
   end
 
-  defp update_account_changeset(%Account{balance: balance} = account, amount) do
+  defp update_account_balance(%Account{balance: balance} = account, amount) do
     Account.changeset(account, Map.new(balance: Money.subtract(balance, amount)))
   end
 
-  defp withdraw_changeset(%Account{} = account, amount) do
-    account
+  defp create_transaction(changes, amount) do
+    changes
+    |> Map.get(:withdrawal_account)
     |> Ecto.build_assoc(:transactions)
     |> Transaction.changeset(Map.new(amount: Money.neg(amount)))
   end

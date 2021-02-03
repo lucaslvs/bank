048039a10f0048f2d630f6b9444fbd6b279aebc3
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-03 01:23:17 -0300

    Add @type in Bank.Financial.Operation.Deposit module

--- lib/bank/financial/operation/deposit.ex
+++ lib/bank/financial/operation/deposit.ex
@@ -6,8 +6,10 @@ defmodule Bank.Financial.Operation.Deposit do
   alias Bank.Customers.Account
   alias Bank.Financial.Transaction
 
+  @type deposit_params() :: %{account: Account.t(), amount: Money.t()}
+
   @impl Bank.Financial.Operation
-  @spec build(%{account: Account.t(), amount: Money.t()}) :: Multi.t()
+  @spec build(deposit_params()) :: Multi.t()
   def build(%{account: %Account{} = account, amount: %Money{} = amount}) do
     Multi.new()
     |> Multi.update(:deposit_account, add_account_balance(account, amount))

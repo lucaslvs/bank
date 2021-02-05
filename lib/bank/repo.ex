d3477a126ea78de4bf78a4fcdb60b0f2fa095dc3
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-05 17:50:08 -0300

    Add use Scrivener in Bank.Repo

--- lib/bank/repo.ex
+++ lib/bank/repo.ex
@@ -3,6 +3,8 @@ defmodule Bank.Repo do
     otp_app: :bank,
     adapter: Ecto.Adapters.Postgres
 
+  use Scrivener, page_size: 10
+
   @impl Ecto.Repo
   def init(_type, config) do
     {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}

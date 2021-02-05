24466148f0e9d1348deb7d1ccab8d4b04abaa9cf
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-05 17:48:48 -0300

    Install Scrivener

--- mix.exs
+++ mix.exs
@@ -56,6 +56,7 @@ defmodule Bank.MixProject do
       {:casex, "~> 0.4.0"},
       {:cors_plug, "~> 2.0"},
       {:bamboo, "~> 1.6"},
+      {:scrivener_ecto, "~> 2.7"},
       {:mock, "~> 0.3.0", only: :test},
       {:ex_machina, "~> 2.4", only: :test},
       {:excoveralls, "~> 0.13.3", only: :test},

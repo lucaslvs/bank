fbed05afcfe4ec5ede6d46d68191baa941ee276b
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-05 17:47:17 -0300

    Creates Bank.Financial.Transaction.QueryBuilder module

---
+++ lib/bank/financial/transaction/query_builder.ex
@@ -0,0 +1,30 @@
+defmodule Bank.Financial.Transaction.QueryBuilder do
+  @moduledoc false
+
+  import Ecto.Query, warn: false
+
+  alias Bank.Financial.Transaction
+
+  @spec filter(map()) :: Ecto.Query.t()
+  def filter(params) when is_map(params) do
+    Transaction
+    |> order_by(^filter_order_by(params["order_by"]))
+    |> where(^filter_where(params))
+  end
+
+  defp filter_order_by("inserted_at"), do: dynamic([t], t.inserted_at)
+
+  defp filter_order_by("inserted_at_desc"), do: [desc: dynamic([t], t.inserted_at)]
+
+  defp filter_order_by(_), do: []
+
+  defp filter_where(params) do
+    Enum.reduce(params, dynamic(true), fn
+      {"inserted_at", inserted_at}, dynamic ->
+        dynamic([t], ^dynamic and t.inserted_at == ^inserted_at)
+
+      {_, _}, dynamic ->
+        dynamic
+    end)
+  end
+end

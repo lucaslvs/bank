ea9daed865b9b5bf35ff2b212642ff99f35ef877
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-09 17:19:02 -0300

    Add test for Bank.filter_transactions/1 in Bank.FinancialTest

--- test/bank/financial_test.exs
+++ test/bank/financial_test.exs
@@ -34,6 +34,44 @@ defmodule Bank.FinancialTest do
     end
   end
 
+  describe "filter_transactions/1" do
+    setup [:create_user, :create_account]
+
+    test "Returns a number of transactions by the given page_size value filter value", %{
+      account: account
+    } do
+      [transaction | _transactions] = insert_list(2, :transaction, account: account)
+
+      assert {:ok, page} = Financial.filter_transactions(Map.new(page_size: 1))
+      assert page.entries == [transaction]
+      assert page.page_number == 1
+      assert page.page_size == 1
+      assert page.page_total_amount == "R$ 100.00"
+      assert page.total_amount == "R$ 200.00"
+      assert page.total_entries == 2
+      assert page.total_pages == 2
+    end
+
+    test "Returns a page of transactions by the given page value filter value", %{
+      account: account
+    } do
+      [_transaction | transaction] = insert_list(2, :transaction, account: account)
+
+      assert {:ok, page} = Financial.filter_transactions(Map.new(page: 2, page_size: 1))
+      assert page.entries == transaction
+      assert page.page_number == 2
+      assert page.page_size == 1
+      assert page.page_total_amount == "R$ 100.00"
+      assert page.total_amount == "R$ 200.00"
+      assert page.total_entries == 2
+      assert page.total_pages == 2
+    end
+
+    # test "Returns a page of transactions by the given insert_from filter value", %{account: account} do
+    #   [_transaction | transaction] = insert_list(2, :transaction, account: account)
+    # end
+  end
+
   defp create_user(_context), do: {:ok, user: insert(:user)}
 
   defp create_account(%{user: user}), do: {:ok, account: insert(:account, user: user)}

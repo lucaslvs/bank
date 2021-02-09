f4e435ef68738e870f45e75acaff51f7075fdde0
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-09 16:31:41 -0300

    Add transactions in Bank.Factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -3,7 +3,10 @@ defmodule Bank.Factory do
 
   use ExMachina.Ecto, repo: Bank.Repo
 
+  import Money.Sigils
+
   alias Bank.Customers.{Account, User}
+  alias Bank.Financial.Transaction
 
   def user_factory do
     %User{
@@ -21,13 +24,29 @@ defmodule Bank.Factory do
     }
   end
 
-  defp get_user_id(params)
-
-  defp get_user_id(%{user_id: user_id}) when is_integer(user_id) do
-    user_id
+  def transaction_factory(params) do
+    %Transaction{
+      amount: ~M[100_00],
+      account_id: get_account_id(params),
+      type: get_type(params)
+    }
   end
+
+  def get_type(params) do
+    types = [:withdraw, :deposit, :transfer_deposit, :transfer_withdrawal]
+
+    Map.get(params, :type, sequence(:type, types))
+  end
+
+  defp get_account_id(params)
+
+  defp get_account_id(%{account: %Account{id: account_id}}), do: account_id
+
+  defp get_account_id(_params), do: raise(":account is required")
+
+  defp get_user_id(params)
 
   defp get_user_id(%{user: %User{id: user_id}}), do: user_id
 
-  defp get_user_id(_params), do: raise(":user or :user_id is required")
+  defp get_user_id(_params), do: raise(":user is required")
 end

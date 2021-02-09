250e4e26538cc7889aca18c5bfa13aaaeab0f291
Lucas Varlesse <lucas.varlesse@redealumni.com>
2021-02-09 16:34:41 -0300

    Add test for Bank.filter_transactions/0 in Bank.FinancialTest

--- test/bank/financial_test.exs
+++ test/bank/financial_test.exs
@@ -1,70 +1,40 @@
 defmodule Bank.FinancialTest do
   use Bank.DataCase
 
+  import Bank.Factory
+
   alias Bank.Financial
+  alias Bank.Financial.Transaction
 
-  describe "transactions" do
-    alias Bank.Financial.Transaction
+  describe "filter_transactions/0" do
+    setup [:create_user, :create_account]
 
-    @valid_attrs %{amount: 42}
-    @update_attrs %{amount: 43}
-    @invalid_attrs %{amount: nil}
-
-    def transaction_fixture(attrs \\ %{}) do
-      {:ok, transaction} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Financial.create_transaction()
-
-      transaction
+    test "Returns a page with empty entries when there isn't transactions" do
+      assert {:ok, page} = Financial.filter_transactions()
+      assert page.entries == []
+      assert page.page_number == 1
+      assert page.page_size == 20
+      assert page.page_total_amount == "R$ 0.00"
+      assert page.total_amount == "R$ 0.00"
+      assert page.total_entries == 0
+      assert page.total_pages == 1
     end
 
-    test "list_transactions/0 returns all transactions" do
-      transaction = transaction_fixture()
-      assert Financial.list_transactions() == [transaction]
-    end
+    test "Returns a page with all existing transactions", %{account: account} do
+      transactions = insert_list(3, :transaction, account: account)
 
-    test "get_transaction!/1 returns the transaction with given id" do
-      transaction = transaction_fixture()
-      assert Financial.get_transaction!(transaction.id) == transaction
-    end
-
-    test "create_transaction/1 with valid data creates a transaction" do
-      assert {:ok, %Transaction{} = transaction} = Financial.create_transaction(@valid_attrs)
-      assert transaction.amount == 42
-    end
-
-    test "create_transaction/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Financial.create_transaction(@invalid_attrs)
-    end
-
-    test "update_transaction/2 with valid data updates the transaction" do
-      transaction = transaction_fixture()
-
-      assert {:ok, %Transaction{} = transaction} =
-               Financial.update_transaction(transaction, @update_attrs)
-
-      assert transaction.amount == 43
-    end
-
-    test "update_transaction/2 with invalid data returns error changeset" do
-      transaction = transaction_fixture()
-
-      assert {:error, %Ecto.Changeset{}} =
-               Financial.update_transaction(transaction, @invalid_attrs)
-
-      assert transaction == Financial.get_transaction!(transaction.id)
-    end
-
-    test "delete_transaction/1 deletes the transaction" do
-      transaction = transaction_fixture()
-      assert {:ok, %Transaction{}} = Financial.delete_transaction(transaction)
-      assert_raise Ecto.NoResultsError, fn -> Financial.get_transaction!(transaction.id) end
-    end
-
-    test "change_transaction/1 returns a transaction changeset" do
-      transaction = transaction_fixture()
-      assert %Ecto.Changeset{} = Financial.change_transaction(transaction)
+      assert {:ok, page} = Financial.filter_transactions()
+      assert page.entries == transactions
+      assert page.page_number == 1
+      assert page.page_size == 20
+      assert page.page_total_amount == "R$ 300.00"
+      assert page.total_amount == "R$ 300.00"
+      assert page.total_entries == 3
+      assert page.total_pages == 1
     end
   end
+
+  defp create_user(_context), do: {:ok, user: insert(:user)}
+
+  defp create_account(%{user: user}), do: {:ok, account: insert(:account, user: user)}
 end
